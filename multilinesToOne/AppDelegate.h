f401d551bb4651ad5d74d4538bf89ecf26039f4e
Martin Magakian <martin.magakian@gmail.com>
2013-08-03 18:27:37 +0700

    Initial Commit

---
+++ multilinesToOne/AppDelegate.h
@@ -0,0 +1,19 @@
+//
+//  AppDelegate.h
+//  multilinesToOne
+//
+//  Created by martin on 03/08/2013.
+//  Copyright (c) 2013 doduck.com. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@class ViewController;
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (strong, nonatomic) ViewController *viewController;
+
+@end
---
+++ multilinesToOne/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  multilinesToOne
+//
+//  Created by martin on 03/08/2013.
+//  Copyright (c) 2013 doduck.com. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
